ac69182b8106d946b320d7dc4c8d29afcbad003a
ADEL KARA <karabesch@gmail.com>
2023-12-09 11:33:32 +0100

    ajout du css pour App et Index et test d'affichage du composant Card dans App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  return <div></div>;
+  const handleClick = (id) => {
+    console.log(`Clicked card with ID: ${id}`);
+  };
+
+  const cardData = {
+    id: 1,
+    src: "/public/jay.png",
+    matched: false,
+  };
+
+  return (
+    <div className="App">
+      <h1>Test de composant Card</h1>
+      <Card cardData={cardData} handleClick={handleClick} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import "./Card.css";
+
 const Card = ({ cardData, handleClick }) => {
   const { id, src, matched } = cardData;
 
