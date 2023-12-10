5a161a2ceafeb7ac5957af536ff444dda6912132
ADEL KARA <karabesch@gmail.com>
2023-12-10 20:20:25 +0100

    création du composant Bingo.jsx et Bingo.ss

---
+++ src/components/Bingo.jsx
@@ -0,0 +1,16 @@
+import "./Bingo.css";
+
+const Bingo = () => {
+  return (
+    <div className="bingo-container">
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <h3 className="animate-charcter">BINGO</h3>
+          <p>Vous êtes un super ninja !!!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bingo;
